494cd85f7dd56de751d491f7fd8b9c7d54d6877f
muzayyinul <muzayyinulf@gmail.com>
2017-10-22 21:57:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ardyatmika/penampilweb/ConnectionCheck.java
@@ -0,0 +1,41 @@
+package com.example.ardyatmika.penampilweb;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by ardyatmika on 22/10/2017.
+ */
+
+public class ConnectionCheck {
+
+    Context ctx ;
+
+    public ConnectionCheck(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    // Check Connection Network
+    public boolean isConnectingToNetwork()
+    {
+        ConnectivityManager connectivity =
+                (ConnectivityManager) ctx.getApplicationContext()
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivity != null)
+        {
+            NetworkInfo[] info= connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+        }
+        return false;
+    }
+
+
+}
+
